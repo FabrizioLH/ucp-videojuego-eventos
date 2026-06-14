51e5367e5397e7b5bb6d5b8091231302eccbcfe8
FabrizioLH <168605254+FabrizioLH@users.noreply.github.com>
2026-06-14 18:48:28 -0300

    imagenes png para PWA

--- js/sw.js
+++ js/sw.js
@@ -1,17 +1,17 @@
-const CACHE_NAME = 'reaccion-rapida-v3';
+const CACHE_NAME = 'reaccion-rapida-v4';
 const ASSETS_TO_CACHE = [
   'index.html',
   'css/styles.css',
   'js/events.js',
   'js/game.js',
   'manifest.json',
-  'icon.svg',
-  'icon-maskable.svg',
-  'screenshot-desktop.svg',
-  'screenshot-mobile.svg'
+  'icon-192.png',
+  'icon-512.png',
+  'screenshot-desktop.png',
+  'screenshot-mobile.png'
 ];
 
-// Evento de Instalación: Se descargan los componentes estáticos esenciales
+// Evento de Instalación
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -21,7 +21,7 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Evento de Activación: Limpieza de versiones obsoletas de caché
+// Evento de Activación
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
@@ -37,9 +37,8 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Evento Fetch: Intercepta peticiones para servir contenido offline
+// Evento Fetch (Omite WebSockets del ranking en tiempo real)
 self.addEventListener('fetch', (event) => {
-  // CRÍTICO: Ignorar peticiones de WebSockets (wss://) o APIs externas para no romper el ranking RT de la UCP
   if (event.request.url.startsWith('ws://') || event.request.url.startsWith('https://gamehubmanager.azurewebsites.net') || event.request.url.startsWith('wss://')) {
     return; 
   }
@@ -47,26 +46,20 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
       if (cachedResponse) {
-        return cachedResponse; // Retorna desde la caché si existe
+        return cachedResponse;
       }
-
-      // De lo contrario, busca en la red
       return fetch(event.request).then((networkResponse) => {
-        // Validamos que sea una respuesta correcta antes de guardarla dinámicamente
         if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
           return networkResponse;
         }
-
         const responseToCache = networkResponse.clone();
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache);
         });
-
         return networkResponse;
       }).catch(() => {
-        // Si falla la red y no está en caché (ej. una subpágina o recurso nuevo estando offline)
         if (event.request.mode === 'navigate') {
-          return caches.match('./index.html');
+          return caches.match('index.html');
         }
       });
     })
